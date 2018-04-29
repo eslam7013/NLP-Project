c643871ce1176ab6576eee85bb0d3b8fe5eaa931
eslam7013 <eslamfigo7013@gmail.com>
2018-04-29 14:17:14 +0200

    adding files

---
+++ Language.py
@@ -0,0 +1,173 @@
+from __future__ import print_function
+import os
+import pickle
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import WordPunctTokenizer
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+from nltk.classify import NaiveBayesClassifier
+from nltk.classify import MaxentClassifier
+
+#region Tokenizing, Stemming and Bigram finder and scores
+def extract_words(text):
+    # For English, although Porter Stemmer is not good
+    # Replace it with morpha (Lemmatizer)
+    stemmer = PorterStemmer()
+    tokenizer = WordPunctTokenizer()
+    tokens = set(tokenizer.tokenize(text))
+    print("tokens : \n-------------")
+    print(tokens)
+    '''Finding collocations requires first calculating the frequencies of words and
+    their appearance in the context of other words. Often the collection of words
+    will then requiring filtering to only retain useful content terms. Each ngram
+    of words may then be scored according to some association measure, in order
+    to determine the relative likelihood of each ngram being a collocation.
+    
+    The ``BigramCollocationFinder`` and ``TrigramCollocationFinder`` classes provide
+    these functionalities, dependent on being provided a function which scores a
+    ngram given appropriate frequency counts. A number of standard association
+    measures are provided in bigram_measures and trigram_measures.
+    '''
+    bigram_finder = BigramCollocationFinder.from_words(tokens)
+    print("Bigram finder : \n--------------------")
+    print(bigram_finder)
+
+    #Scores for bigram tokens
+    bigrams = bigram_finder.nbest(BigramAssocMeasures.chi_sq, 500)
+    for bigram_tuple in bigrams:
+        x = "%s %s" % bigram_tuple
+        tokens.add(x)
+    result = [stemmer.stem(x.lower()) for x in tokens
+              if len(x) > 1]
+    return result
+#endregion
+
+#region Get words in dic
+def get_feature(word):
+    return dict([(word, True)])
+#endregion
+
+#region BOW
+def bag_of_words(words):
+    return dict([(word, True) for word in words])
+#endregion
+
+#region Get Training_Set
+def get_train_set(texts):
+    # Change to buffer `texts`
+    train_set = []
+    for sense, file in texts.items():
+        print("training %s " % sense)
+        text = open(file, 'r',encoding='utf-8').read()  # Change later
+        features = extract_words(text)
+        train_set = train_set + [(get_feature(word), sense) for word in features]
+    return train_set
+#endregion
+
+#region Classifying
+def Language_Identification(text, classifier):
+
+    tokens = bag_of_words(extract_words(text))
+    # printing what is the probability that entered sentence are related to => langugae
+    print(classifier.prob_classify(tokens).prob('en'),"==>en")
+    print(classifier.prob_classify(tokens).prob('es'),"==>es")
+    print(classifier.prob_classify(tokens).prob('fr'),"==>fr")
+    print (classifier.prob_classify(tokens).prob ('ge'), "==>ge")
+    print (classifier.prob_classify(tokens).prob ('ar'), "==>ar")
+    print(classifier.prob_classify(tokens).prob('franko'),"==>franko")
+
+    # classifying the entered sentence
+    decision = classifier.classify(tokens)
+    return decision
+#endregion
+
+#region Classifying file
+def File_Language_Identification(filename, classifier):
+    decision = []
+    with open(filename,'r',encoding="UTF-8") as f :
+        sentences = f.readlines()
+
+    print(sentences)
+    for text in sentences:
+        tokens = bag_of_words(extract_words(text))
+        # printing what is the probability that entered sentence are related to => langugae
+        print(classifier.prob_classify(tokens).prob('en'),"==>en")
+        print(classifier.prob_classify(tokens).prob('es'),"==>es")
+        print(classifier.prob_classify(tokens).prob('fr'),"==>fr")
+        print (classifier.prob_classify(tokens).prob ('de'), "==>ge")
+        print (classifier.prob_classify(tokens).prob ('ar'), "==>ar")
+        print(classifier.prob_classify(tokens).prob('franko'),"==>franko")
+
+        # classifying the entered sentence
+        language = classifier.classify(tokens)
+        decision.append((text,language))
+
+    return decision
+#endregion
+
+#region Prepare the classifier
+def Main(string):
+    domain = "text"
+    classifier_name = "MaxEntropy"
+    texts = {}
+    pickled_classifier = r'E:\NLP_Project\language_identification_classifier-%s.%s.pickle' % (classifier_name, domain)
+
+    if not os.path.exists (pickled_classifier):
+        texts['en'] = 'en.txt'
+        texts['es'] = 'es.txt'
+        texts['fr'] = 'fr.txt'
+        texts['ar'] = 'ar.txt'
+        texts['ge'] = 'de.txt'
+        texts['franko'] = 'franko.txt'
+
+        print ("Training on Files", texts)
+        print ("Will be pickling", pickled_classifier)
+
+        train_set = get_train_set (texts)
+        classifier = MaxentClassifier.train(train_set,max_iter=4)
+        #classifier = NaiveBayesClassifier.train (train_set)
+        pickle.dump (classifier, open (pickled_classifier, 'wb'))
+    else:
+        classifier = pickle.load (open (pickled_classifier, "rb"))
+
+    return File_Language_Identification(string, classifier)
+#endregion
+
+
+# string="انا جايلك يا فراج و هحط عليك"
+# print(Main(string),"==>",string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Language_GUI.py
@@ -0,0 +1,81 @@
+from tkinter import *
+from tkinter import messagebox
+import Language as lang
+
+# region initialize root(empty) window
+root = Tk()
+root.title("Behaviour Analyzer")
+root.geometry("400x150")
+# root.resizable(False, False)
+# root.configure(background='grey')
+# endregion
+
+# region initialize frames (DISABLED)
+# top_frame = Frame(root)
+# top_frame.pack(fill=X)
+# bottom_frame = Frame(root)
+# bottom_frame.pack(side=BOTTOM, fill=X)
+# endregion
+
+# region Top Area
+empty = "        "
+empty_label_1 = Label(root, text=empty)
+empty_label_1.grid(row=0, column=0)
+# create a label (textbox)
+# input_type_label = Label(root, text="Input type :")
+# # put the label in the first possible place
+# input_type_label.grid(row=0, column=1, sticky=E)
+#
+#
+# # input type parameters
+# input_type = StringVar(root)
+# input_type.set("Select required type")
+#
+#
+# def update_input_type_label(in_type):
+#     """ this function handles the update of the label"""
+#     input_type_label.configure(text=in_type + " :")
+#     input_type_label.update()
+#
+# input_type_list = OptionMenu(root, input_type, "Tweet", command=update_input_type_label)
+# input_type_list.grid(row=0, column=2, sticky=E)
+
+# endregion
+
+# region Bottom Area
+empty_label_2 = Label(root, text=empty)
+empty_label_2.grid(row=1, column=0)
+
+input_type_label = Label(root, text="Input :")
+# put the label in the first possible place
+input_type_label.grid(row=1, column=1, sticky=E)
+
+
+# user input field
+user_input = Text(root, height=3, width=30)
+user_input.grid(row=1, column=2)
+
+def submit_data():
+    data = user_input.get("1.0", 'end-1c')
+    print(data)
+
+    # tweet : @mileskimball a second Trump Administration will be in position to attack rural and many urban druggers in Mexico,… https://t.co/dnFF1Pcg4q"
+    #if input_type.get() == "Tweet":
+    result = lang.Main(data)
+    #messagebox.showinfo("Result : " , "Sentence : "+data + "\nLanguage : "+result)
+    print(result)
+    return
+
+
+empty_label_3 = Label(root, text=empty)
+empty_label_3.grid(row=2, column=1)
+empty_label_4 = Label(root, text=empty)
+empty_label_4.grid(row=3, column=0)
+# submit button
+submit_button = Button(root, text="Submit", command=submit_data)
+submit_button.grid(row=3, column=2)
+
+# endregion
+
+# create GUI loop
+root.mainloop()
